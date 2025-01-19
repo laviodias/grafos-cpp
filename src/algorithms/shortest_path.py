7af306015ec87c26d5d1e856dee5c60b06e77c99
Lávio Vale <laviovale@gmail.com>
2025-01-19 17:14:13 -0300

    Corrigindo problema de abastecer em ponto de destino

--- src/algorithms/shortest_path.py
+++ src/algorithms/shortest_path.py
@@ -1,8 +1,6 @@
 import sys
 from itertools import permutations
 
-
-# Floyd-Warshall com suporte para predecessores
 def floyd_warshall_with_predecessors(adj_matrix):
     n = len(adj_matrix)
     dist = [[sys.maxsize] * n for _ in range(n)]
@@ -26,8 +24,6 @@ def floyd_warshall_with_predecessors(adj_matrix):
 
     return dist, pred
 
-
-# Reconstrói o caminho mínimo usando a matriz de predecessores
 def reconstruct_path(pred, start, end):
     if pred[start][end] == -1:
         return None
@@ -39,14 +35,16 @@ def reconstruct_path(pred, start, end):
     path.reverse()
     return path
 
-
-# Função para calcular o caminho mínimo com restrições de combustível
 def find_path_with_fuel_limit(adj_matrix, start, mandatory, fuel_limit):
     dist, pred = floyd_warshall_with_predecessors(adj_matrix)
     best_path = None
     min_cost = sys.maxsize
     min_stops = sys.maxsize
     min_visited_stops = []
+    reason = None
+
+    all_nodes = set(range(len(adj_matrix)))
+    refuel_nodes = list(all_nodes - set(mandatory) - {start})
 
     for perm in permutations(mandatory):
         current_path = [start]
@@ -56,35 +54,79 @@ def find_path_with_fuel_limit(adj_matrix, start, mandatory, fuel_limit):
         visited_stops = []
         valid_path = True
 
-        for i in range(len(perm)):
-            next_vertex = perm[i]
-            path_segment = reconstruct_path(pred, current_path[-1], next_vertex)
+        remaining_mandatory = list(perm)
+        current_node = start
 
-            if path_segment is None:
+        while remaining_mandatory:
+            next_vertex = None
+            min_cost_to_next = sys.maxsize
+            best_refuel_path = None
+            best_path_to_next = None
+
+            for next_mand in remaining_mandatory:
+                path_to_next = reconstruct_path(pred, current_node, next_mand)
+                if path_to_next:
+                    cost_to_next = 0
+                    for l in range(1, len(path_to_next)):
+                        cost_to_next += adj_matrix[path_to_next[l - 1]][path_to_next[l]]
+                    if current_fuel + cost_to_next <= fuel_limit:
+                        if cost_to_next < min_cost_to_next:
+                            min_cost_to_next = cost_to_next
+                            next_vertex = next_mand
+                            best_path_to_next = path_to_next
+                    else:
+                        for refuel_node in refuel_nodes:
+                            path_to_refuel = reconstruct_path(pred, current_node, refuel_node)
+                            if path_to_refuel:
+                                refuel_cost = 0
+                                for l in range(1, len(path_to_refuel)):
+                                    refuel_cost += adj_matrix[path_to_refuel[l - 1]][path_to_refuel[l]]
+
+                                if current_fuel + refuel_cost <= fuel_limit:
+                                    path_from_refuel_to_next = reconstruct_path(pred, refuel_node, next_mand)
+                                    if path_from_refuel_to_next:
+                                        from_refuel_cost = 0
+                                        for l in range(1, len(path_from_refuel_to_next)):
+                                            from_refuel_cost += adj_matrix[path_from_refuel_to_next[l - 1]][
+                                                path_from_refuel_to_next[l]]
+                                        if from_refuel_cost <= fuel_limit:
+                                            if refuel_cost + from_refuel_cost < min_cost_to_next:
+                                                min_cost_to_next = refuel_cost + from_refuel_cost
+                                                next_vertex = next_mand
+                                                best_refuel_path = path_to_refuel
+                                                best_path_to_next = path_from_refuel_to_next
+
+            if next_vertex is None:
                 valid_path = False
+                reason = f"Não foi possível encontrar um caminho para nenhum dos nós obrigatórios."
                 break
 
+            if best_refuel_path:
+                current_path.extend(best_refuel_path[1:])
+                current_fuel = 0
+                stops_count += 1
+                visited_stops.append(best_refuel_path[-1])
+
+            path_segment = reconstruct_path(pred, current_node, next_vertex)
             for j in range(1, len(path_segment)):
-
                 weight = adj_matrix[path_segment[j - 1]][path_segment[j]]
-                if current_fuel + weight > fuel_limit:
-                    if current_path[-1] != path_segment[j - 1]:
-                        current_path.append(path_segment[j - 1])
-                    current_fuel = 0
-                    stops_count += 1
-                    visited_stops.append(path_segment[j - 1])
-
-                current_path.append(path_segment[j])
                 current_fuel += weight
                 total_cost += weight
+            current_path.append(path_segment[-1])
 
-        if valid_path and current_fuel <= fuel_limit:
-            if total_cost < min_cost:
-                min_cost = total_cost
-                min_stops = stops_count
-                best_path = current_path
-                min_visited_stops = visited_stops
+            current_node = next_vertex
+            remaining_mandatory.remove(next_vertex)
+
+        if valid_path and (len(visited_stops) < min_stops or (len(visited_stops) == min_stops and total_cost < min_cost)):
+            min_cost = total_cost
+            min_stops = stops_count
+            best_path = current_path
+            min_visited_stops = visited_stops
+            reason = None
 
     if best_path is None:
+        print("Não foi possível encontrar um caminho válido.")
+        if reason:
+            print(f"Motivo: {reason}")
         return None, 0, 0
     return best_path, min_cost, min_visited_stops
